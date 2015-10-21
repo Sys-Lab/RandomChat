fa1442116ff5b13f4cbfcd8b770a4a891f790571
lc4t <lc4t0.0@gmail.com>
2015-10-22 00:37:02 +0800

    add php files: index login footer header

---
+++ html/login.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lc4t
+ * Date: 10/22/15
+ * Time: 12:02 AM
+ * function login
+ */
+
+    if (isset($_POST['username']) && isset($_POST['password']))
+    {
+        //raw: the raw input from user,
+        $raw_username = $_POST['username'];
+        $raw_password = $_POST['password'];
+    }
+    else
+    {
+        die('input error');
+    }
---
+++ include/footer.php
@@ -0,0 +1,5 @@
+<?php
+    //This is footer.php content.This file must be use by require.
+    echo '<!-- footer content -->';
+
+?>
